82d0e9f6bdbbe0c6e7eda81c6513480333361c35
Ivan Nezgovorov <87833835+IvanNez@users.noreply.github.com>
2024-07-29 13:12:23 +0500

    init project

---
+++ ElectroniceShop/ElectroniceShop/ElectroniceShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroniceShopApp.swift
+//  ElectroniceShop
+//
+//  Created by Иван Незговоров on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ElectroniceShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
